7bcbc39090af26ecdd56a9d51250bdcff0e9a0eb
Кияница Денис <kiyanitsadenis@gmail.com>
2022-12-07 23:55:20 +0700

    add get command and panic recover for it

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"github.com/Kiatsyndesi/tg_bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 )
 
 type Commander struct {
@@ -14,5 +15,28 @@ func NewCommander(bot *tgbotapi.BotAPI, productService *product.Service) *Comman
 	return &Commander{
 		bot:            bot,
 		productService: productService,
+	}
+}
+
+func (c *Commander) HandleUpdate(update tgbotapi.Update) {
+	defer func() {
+		if panicValue := recover(); panicValue != nil {
+			log.Printf("Value from recovered panic: %v", panicValue)
+		}
+	}()
+
+	if update.Message == nil {
+		return
+	}
+
+	switch update.Message.Command() {
+	case "help":
+		c.Help(update.Message)
+	case "list":
+		c.List(update.Message)
+	case "get":
+		c.Get(update.Message)
+	default:
+		c.Default(update.Message)
 	}
 }
